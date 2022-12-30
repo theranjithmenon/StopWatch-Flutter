fd3e7434d47ca58df7e376b493de96ad8160429b
Ranjith Menon <60739917+theranjithmenon@users.noreply.github.com>
2022-12-30 11:37:17 +0530

    buttons

--- lib/page.dart
+++ lib/page.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:lottie/lottie.dart';
 
 class StopWatch extends StatefulWidget {
@@ -13,6 +12,7 @@ class StopWatch extends StatefulWidget {
 }
 
 class _StopWatchState extends State<StopWatch> {
+  bool isRunning = false;
   Duration duration = Duration();
   Timer? timer;
   bool isCountdown = true;
@@ -32,6 +32,8 @@ class _StopWatchState extends State<StopWatch> {
     timer = Timer.periodic(Duration(seconds: 1), (timer) => addTime());
   }
 
+  void stopTimer() {}
+
   void addTime() {
     setState(() {
       final seconds = duration.inSeconds + 1;
@@ -41,7 +43,6 @@ class _StopWatchState extends State<StopWatch> {
 
   @override
   Widget build(BuildContext context) {
-    bool isRunning = false;
     String twoDigits(int n) => n.toString().padLeft(2, '0');
     final hours = twoDigits(duration.inHours);
     final minutes = twoDigits(duration.inMinutes.remainder(60));
@@ -60,23 +61,49 @@ class _StopWatchState extends State<StopWatch> {
             '$hours: $minutes : $seconds',
             style: const TextStyle(fontSize: 55),
           ),
-          MaterialButton(
-            color: Colors.lightBlueAccent,
-            shape: const CircleBorder(),
-            onPressed: () {
-              startTimer();
-              setState(() {
-                isRunning = true;
-              });
-            },
-            child: const Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Icon(
-                Icons.play_arrow_outlined,
-                size: 50,
-              ),
-            ),
-          ),
+          (isRunning)
+              ? MaterialButton(
+                  color: Colors.redAccent,
+                  shape: const CircleBorder(),
+                  onPressed: () {
+                    // reset();
+                    timer?.cancel();
+                    setState(() {
+                      isRunning = false;
+                    });
+                  },
+                  onLongPress: () {
+                    reset();
+                    timer?.cancel();
+                    setState(() {
+                      isRunning = false;
+                    });
+                  },
+                  child: const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Icon(
+                      Icons.stop,
+                      size: 50,
+                    ),
+                  ),
+                )
+              : MaterialButton(
+                  color: Colors.lightBlueAccent,
+                  shape: const CircleBorder(),
+                  onPressed: () {
+                    startTimer();
+                    setState(() {
+                      isRunning = true;
+                    });
+                  },
+                  child: const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Icon(
+                      Icons.play_arrow_outlined,
+                      size: 50,
+                    ),
+                  ),
+                ),
         ],
       ))),
     );
